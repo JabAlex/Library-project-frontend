7a992114c763da22bf7f7d92862c44dcbe0d2827
Aleksander Jablonski <jablonski.aleksander98@gmail.com>
2023-05-25 19:38:16 +0200

    first commit

---
+++ src/main/java/com/libraryproject/LibraryProjectFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.libraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectFrontendApplication.class, args);
+	}
+
+}
